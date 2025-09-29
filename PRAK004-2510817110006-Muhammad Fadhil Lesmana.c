6b1324c4c27512bf26bb80a7ef2049128a304c64
MuhammadFadhilLesmana <fadhillesmana45@gmail.com>
2025-09-30 05:53:04 +0800

    PRAK004-2510817110006-Muhammad Fadhil Lesmana.c

---
+++ PRAK004-2510817110006-Muhammad Fadhil Lesmana.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("#############################\n");
+    printf("#                           #\n");
+    printf("#  \033[1;31mMUHAMMAD FADHIL LESMANA\033[0m  #\n");
+    printf("#       \033[1;31m2510817110006\033[0m       #\n");
+    printf("#                           #\n");
+    printf("#############################\n");
+    return 0;
+}
