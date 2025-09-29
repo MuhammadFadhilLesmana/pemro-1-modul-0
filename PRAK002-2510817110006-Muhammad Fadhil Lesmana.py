d7a9d16e65dee42e77b7c085dba6aca7b8bd619b
MuhammadFadhilLesmana <fadhillesmana45@gmail.com>
2025-09-30 05:42:32 +0800

    PRAK002-2510817110006-Muhammad Fadhil Lesmana.py

---
+++ PRAK002-2510817110006-Muhammad Fadhil Lesmana.py
@@ -0,0 +1,3 @@
+red = "\033[1;31m"
+reset = "\033[0m"
+print (f"Selamat Pagi, {red}MUHAMMAD FADHIL LESMANA {reset}\nSelamat Siang, {red}MUHAMMAD FADHIL LESMANA{reset}\nSelamat Malam, {red}MUHAMMAD FADHIL LESMANA{reset}")
