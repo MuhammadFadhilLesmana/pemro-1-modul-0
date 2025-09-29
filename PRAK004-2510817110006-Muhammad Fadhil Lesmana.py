b0079cbf272910ab81ff135f77059fdf517d0d19
MuhammadFadhilLesmana <fadhillesmana45@gmail.com>
2025-09-30 05:51:56 +0800

    PRAK004-2510817110006-Muhammad Fadhil Lesmana.py

---
+++ PRAK004-2510817110006-Muhammad Fadhil Lesmana.py
@@ -0,0 +1,10 @@
+red = "\033[1;31m"
+reset = "\033[0m"
+
+print("#############################")
+print("#                           #")
+print(f"#  {red}MUHAMMAD FADHIL LESMANA{reset}  #")
+print(f"#       {red}2510817110006{reset}       #")
+print("#                           #")
+print("#############################")
+
